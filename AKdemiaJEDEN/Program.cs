4761c3f97e6aa61c0f694c927d8409e4d33c156e
XRamaR <luk.jarnut@gmail.com>
2019-11-06 08:00:54 +0100

    pierwszy commit

---
+++ AKdemiaJEDEN/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKdemiaJEDEN
+{
+    /*przydatne skróty: 
+     * alt+enter ("program mówi nam jak to co robimy zrobić lepiej")
+     * ctrl+spacja (podpowiedź np. metody klasy)
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rowerList = new List<Rower_Klauss>();
+            //Rower_Klauss rowerA = new Rower_Klauss(); //tworzenie nowego obiektu klasy
+            //var rowerA = new Rower_Klauss(); //to samo co linijke wyzej
+            var rowerA = new Rower_Klauss(0.5f);  //to samo utworzenie co wyzej po wykosztaniu ALt+Enter
+            var rowerB = new Rower_Klauss(0.8f);
+
+            rowerList.Add(rowerA);
+            rowerList.Add(rowerB);
+
+            for (int i = 0; i < rowerList.Count; i++) //podwójnie tab po "for" dla prefixu. Wypisanie całej tablicy
+            {
+                Console.WriteLine(rowerList[i]);
+            }
+
+            foreach(var rower in rowerList) //jw
+            {
+                string msg = (rower != null) ? rower.ToString() : "nie ma roweru."; //jezeli rower to nie null to wartośc rowera to "nie ma roweru"
+                Console.WriteLine(rower);
+            }
+            //Console.WriteLine("Rower dist: "+ rowerA.dist); //wyświetlanie tekstu i danych
+            //Console.WriteLine($"Rower dist: {rowerA.dist}"); //j.w.
+            //Console.WriteLine(rowerA); //test
+            //Console.WriteLine("Rower dist: " + rowerA.dist.ToString()); //co zrobił kompilator linie wyżej
+
+        }
+    }
+}
---
+++ AKdemiaJEDEN/Rower_Klauss.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKdemiaJEDEN
+{
+    class Rower_Klauss
+    {
+        public int wheel_count;
+        private Color color;
+        public float dist;
+
+        public Rower_Klauss(float dist) //musi być public/private przed każdą funkcją
+        {
+            this.dist = dist;
+            wheel_count = 2;
+        }
+
+        public void Move(float delta)
+        {
+            dist += delta;
+        }
+        public override string ToString() //override nadpisuje domyslne metody
+        {
+            return $"Rower dist: {this.dist}";
+        }
+    }
+}
